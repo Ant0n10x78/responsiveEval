b2d41e6e8ed07940f208a2e88d67eb30c40b83f1
Ant0n10x78 <antonio.gernacaleixo@ynov.com>
2021-05-31 20:21:03 +0200

    update bg

--- rendu/js/main.js
+++ rendu/js/main.js
@@ -1,27 +1,11 @@
-var burgerMenu = document.querySelector('.burger_menu');
-var headerMenu = document.querySelector(".header_menu");
-var headerOverlay = document.querySelector('.menu_overlay');
-var body = document.querySelector('body');
+var burger_btn_on = document.getElementsByClassName(".burger_menu");
+var burger_btn_off = document.getElementsByClassName(".burger_menu");
+var burger_menu = document.getElementsByClassName(".burger_menu_on");
 
-console.log(burgerMenu, 'burger menu');
+burger_btn_on.onclick = function() {
+     burger_menu.style.display = "unset";
+    };
 
-//Fonction à utiliser
-function toggleBurgerMenu(burger, menu, overlay) {
-    burger.classList.toggle('active');
-    menu.classList.toggle('active');
-    overlay.classList.toggle('active');
-    body.classList.toggle('stop-scroll');
-}
-
-burgerMenu.onclick = function () {
-    toggleBurgerMenu(burgerMenu, headerMenu, headerOverlay);
-}
-
-headerOverlay.onclick = function () {
-    toggleBurgerMenu(burgerMenu, headerMenu, headerOverlay);
-}
-
-burgerMenu.addEventListener('click', function () {
-     console.log('test');
-    toggleBurgerMenu();
-})
+burger_btn_off.onclick = function(){
+    burger_menu.style.display = "none";
+};
